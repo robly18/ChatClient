296b42b287a2e6a88b6007b348ddf50696946190
Robly18 <duarteluis.nascimento@hotmail.com>
2015-06-10 16:58:51 +0100

    Namespaces + shenanigans

--- ChatClient/chat/urls.py
+++ ChatClient/chat/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^(?P<roomName>[a-zA-Z0-9_]+)/$', views.chatRoom, name='chatRoom'),
-    url(r'^(?P<roomName>[a-zA-Z0-9_]+)/sendmsg/$', views.msgInput, name='msgInput'),
+    url(r'^room/(?P<roomName>[a-zA-Z0-9_]+)/$', views.chatRoom, name='chatRoom'),
+    url(r'^sendmsg/$', views.msgInput, name='msgInput'),
 ]
--- ChatClient/chat/views.py
+++ ChatClient/chat/views.py
@@ -5,7 +5,9 @@ from django.http import HttpResponse
 # Create your views here.
 
 def chatRoom(request, roomName):
-	return render(request, "chat/chatRoom.html")
+	return render(request, "chat/chatRoom.html", {'roomName':roomName})
 
 def msgInput(request):
+	print("Got a message input!!!")
+	print(request.POST['msgInput'])
 	pass
